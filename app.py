7e3c46f9da1d20713da7bf414704be2542d3282a
direncintelia <84112323+direncintelia@users.noreply.github.com>
2024-03-08 16:51:19 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
 import pandas as pd
 
-st.title('My First Streamlit App')
+st.title('intelia Test App')
 
 # Load a sample dataset
 df = pd.read_csv("https://raw.githubusercontent.com/direncintelia/streamlitdemo/main/your%20data.csv")
 
 # Display the dataframe 
 st.write(df)
+
+
+values = st.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
 
