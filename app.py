7395798112eccb1782a725c767a332f8bd6a58e5
direncintelia <84112323+direncintelia@users.noreply.github.com>
2024-03-08 16:54:59 +1100

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,25 @@ import pandas as pd
 st.title('intelia Test App')
 
 # Load a sample dataset
-df = pd.read_csv("https://raw.githubusercontent.com/direncintelia/streamlitdemo/main/your%20data.csv")
+def get_data():
+    return pd.read_csv("https://raw.githubusercontent.com/direncintelia/streamlitdemo/main/your%20data.csv")
 
 # Display the dataframe 
-st.write(df)
+df = get_data()
 
+min_year = int(df['price'].min())
+max_year = int(df['price'].max())
+
+countries = df['country'].unique()
+
+'## By country'
+country = st.selectbox('country', countries)
+df[df['country'] == country]
+
+
+'## By price'
+price = st.slider('price', min_price, max_price)
+df[df['Price'] == price]
 
 values = st.slider(
     'Select a range of values',
