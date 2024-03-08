754ffc980d4b95097b84d39cc4f117b72a087130
direncintelia <84112323+direncintelia@users.noreply.github.com>
2024-03-08 16:39:55 +1100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title('My First Streamlit App')
 
 # Load a sample dataset
-df = pd.read_csv("your_data.csv")
+df = pd.read_csv("https://github.com/direncintelia/streamlitdemo/blob/main/your%20data.csv")
 
 # Display the dataframe 
 st.write(df)
